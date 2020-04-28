592dc58628df26527bf42d04c9274d6a6cf5e0ed
KunEui <kuney930216@gmail.com>
2020-04-28 15:50:08 -0400

    password info protection

---
+++ Models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        trim: true, //스페이스를 없애주는 역할
+        unique: 1
+    },
+    password: {
+        type: String,
+        minlength: 5
+    },
+    lastname: {
+        type: String,
+        maxlength: 50
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+
+const User = mongoose.model('User', userSchema) //스키마를 모델에 감싸준다.
+module.exports = { User } // 유저를 다른 폴더에도 쓰이게끔 만든다.
